a85981c2860a3f9393050cadc5f514e50c5ff358
ralphusmcgee <38622375+ralphusmcgee@users.noreply.github.com>
2022-12-10 19:09:09 -0500

    Added some stuff

---
+++ ChordWriter/src/Arm.h
@@ -0,0 +1,19 @@
+#ifndef Chord_h
+#define Chord_h
+
+class Arm {
+    private:
+        int num;
+
+    public:
+        Arm(int num);
+        int neutral[3];
+        int A_1[3];
+        int A_2[3];
+        int A_3[3];
+        int A_4[3];
+        int A_5[3];
+        int A_6[3];
+};
+
+#endif
