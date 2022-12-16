acb802144ecf2c6a879906384b39f301efcff01c
ralphusmcgee <38622375+ralphusmcgee@users.noreply.github.com>
2022-12-16 15:46:20 -0500

    Cleaned out unecessary folders

--- HU3910/src/Play.cpp
+++
@@ -1,7 +0,0 @@
-#include "Play.h"
-
-Play::Play (Chord &chord, int duration, int strumDirection) : chord(chord), duration(duration), strumDirection(strumDirection){
-      this->chord = chord;
-      this->duration = duration; 
-      this->strumDirection = strumDirection;
-}
